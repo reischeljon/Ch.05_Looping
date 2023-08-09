4b36eaaf1f9de1c78b94c81215dccb87f3152bbe
Marc Hermon <hermonm@urbandaleschools.com>
2023-08-09 16:40:14 -0500

    Update 5.0_Jedi_Training.py

--- 5.0_Jedi_Training.py
+++ 5.0_Jedi_Training.py
@@ -1,4 +1,4 @@
-# 5.0 Jedi Training (20pts)  Name:________________
+# 5.0 Jedi Training (50pts)  Name:________________
 
 '''
  1. Make the following program work. (5pts)
@@ -43,14 +43,49 @@
 
 
 '''
-  5. Write a Python program that will:
+  5. 7 NUMBER ANALYSIS (5pts)
      
      * Ask the user for seven numbers
      * Print the total sum of the numbers
      * Print the count of the positive entries, the count of entries equal to zero,
      and the count of negative entries. Use an if, elif, else chain, not just three
-     if statements.  (5pts)
+     if statements.
       
 '''
+
+
+
+
+
+
+'''
+6. COIN TOSS PROGRAM (10pts)
+-----------------
+1.) Create a program that will print a random 0 or 1.
+2.) Instead of 0 or 1, print heads or tails. Do this using if statements. Don't select from a list.
+3.) Add a loop so that the program does this 50 times.
+4.) Create a running total for the number of heads and the number of tails and print the total at the end.
+'''
+
+
+
+
+
+'''
+ROSHAMBO PROGRAM (15pts)
+----------------
+
+Create a program that randomly chooses a 1, 2, or 3.
+Expand the program so it randomly prints rock, paper, or scissors using if statements. Don't select from a list.
+Add to the program so it first asks the user their choice as well as if they want to quit.(1 for rock, 2 for paper, 3 for scissors and 4 for quit)
+I don't want to be asked to quit each time. I will enter a 4 if I want to quit.
+Add conditional statements to figure out who wins and keep the records
+Each round, tell me what the computer chose, what I chose and also if I won, lost or tied.
+When the user quits, print an end game message and their win/loss/tie record
+
+'''
+
+
+
 
 
